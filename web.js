91c247c03fd0bb8fbd8691f4493c08fd0223a65e
Sergey Khval <gluks2@yandex.ru>
2013-07-04 11:54:12 +0300

    Optimized web.js again

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 var hello = fs.readFileSync("index.html").toString("utf-8");
 
 app.get('/', function(request, response) {
-  response.send(str);
+  response.send(hello);
 });
 
 var port = process.env.PORT || 5000;
